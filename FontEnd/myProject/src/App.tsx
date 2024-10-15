82338a0663ddd98c449924f199afc3764ca670d5
anh tien <anhtienb237@gmail.com>
2024-10-15 16:08:05 +0700

    Header Update

--- FontEnd/myProject/src/App.tsx
+++ FontEnd/myProject/src/App.tsx
@@ -1,13 +1,10 @@
+import Header from './components/commons/header/Header';
 import './index.css'
 
 function App() {
-
-
   return (
     <>
-      <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-        <h1 className="text-4xl font-bold text-blue-600">Hello, Tailwind CSS!</h1>
-      </div>
+      <Header></Header>
     </>
   );
 }
--- FontEnd/myProject/tailwind.config.js
+++ FontEnd/myProject/tailwind.config.js
@@ -2,8 +2,13 @@
 export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        commonBlack: "#191B1D",
+        bodyBlack: '#232527',
+        inputBlack: '#111212',   
+      }
+    },
   },
   plugins: [],
 }
-
