485fe8690e114888f69bb292beabb7de50e54808
anh tien <anhtienb237@gmail.com>
2024-10-20 22:36:23 +0700

    Updates

--- FontEnd/myProject/src/configs/Routes/RouteConfig.tsx
+++ FontEnd/myProject/src/configs/Routes/RouteConfig.tsx
@@ -3,21 +3,22 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "../../pages/userPages/publicPages/HomePage";
 import NotFoundPage from "../../pages/404Page/NotFoundPage";
+import Login from "../../components/auths/login/Login";
 
 
 const AppRoutes = () => {
     return (
         <div className="min-h-screen">
-                <Routes>
-                    {/* Public Routes - Các route ra vào thoải mải không ràng buộc*/}
-                    <Route path="/" element={<HomePage />} />
-                    <Route path="/login" element/>
-                    {/* Private Routes - Các route bắt buộc phải Login hoặc với Role gì đấy mới được vào*/}
-                    {/* <Route path="/profile" element={<PrivateRoute component={ProfilePage} />} /> */}
+            <Routes>
+                {/* Public Routes - Các route ra vào thoải mải không ràng buộc*/}
+                <Route path="/" element={<HomePage />} />
+                <Route path="/login" element={<Login />} />
+                {/* Private Routes - Các route bắt buộc phải Login hoặc với Role gì đấy mới được vào*/}
+                {/* <Route path="/profile" element={<PrivateRoute component={ProfilePage} />} /> */}
 
-                    {/* 404 Page - Route hiện page lỗi*/}
-                    <Route path="*" element={<NotFoundPage />} />
-                </Routes>
+                {/* 404 Page - Route hiện page lỗi*/}
+                <Route path="*" element={<NotFoundPage />} />
+            </Routes>
         </div>
     );
 };
